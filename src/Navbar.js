999e887f9330191c6ab1e50904edc0296cadb5a5
P KJ <pkjrockzz@gmail.com>
2023-09-11 19:50:58 +0530

    Change home url

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <nav>
       <ul className="navbar-list">
-        <li className="navbar-item"><Link to="/">Home</Link></li>
+        <li className="navbar-item"><Link to="/portfolio-website">Home</Link></li>
         <li className="navbar-item"><Link to="/projects">Projects</Link></li>
         <li className="navbar-item"><Link to="/experience">Experience</Link></li> 
         <li className="navbar-item"><Link to="/about">About</Link></li>
