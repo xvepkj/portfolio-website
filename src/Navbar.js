63f68971b064de2c6653058d440523dd8c0e1b89
P KJ <pkjrockzz@gmail.com>
2023-09-16 00:03:32 +0530

    Remove PKJ

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,37 @@
-// Navbar.js
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
-  const [activeItem, setActiveItem] = useState(null);
   const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleItemClick = (item) => {
-    setActiveItem(item);
-    // Close the menu on item click if it's open (only for mobile)
-    if (menuOpen) {
-      setMenuOpen(false);
-    }
-  };
-
-  const getItemColor = (item) => {
-    return activeItem === item ? '#E63946' : '#1D3557';
-  };
+  const location = useLocation();
 
   const toggleMenu = () => {
     setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+  };
+
+  // Determine the active item based on the current URL path
+  const getActiveItem = () => {
+    const pathname = location.pathname;
+    if (pathname === '/projects') return 'Projects';
+    else if (pathname === '/experience') return 'Experience';
+    else if (pathname === '/about') return 'About';
+    else return 'Home'; // Default to 'Home' for other paths
+  };
+
+  useEffect(() => {
+    // Set the active item when the component mounts and when the URL changes
+    const activeItem = getActiveItem();
+    setActiveItem(activeItem);
+  }, [location.pathname]);
+
+  const [activeItem, setActiveItem] = useState(getActiveItem());
+
+  // Function to close the menu when an item is clicked
+  const closeMenu = () => {
+    if (window.innerWidth <= 768) {
+      setMenuOpen(false);
+    }
   };
 
   return (
@@ -30,52 +41,28 @@ function Navbar() {
         <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
         <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
         <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
+        <div className={`line ${menuOpen ? 'open' : ''}`}></div> {/* Line under menu button */}
       </div>
 
       {/* Sidebar Menu */}
       <ul className={`navbar-list ${menuOpen ? 'open' : ''}`}>
-      <li className="navbar-item pkj">
-          <Link
-            to="/portfolio-website"
-            onClick={() => handleItemClick('PKJ')}
-            style={{ color: getItemColor('PKJ') }}
-          >
-            PKJ
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link
-            to="/portfolio-website"
-            onClick={() => handleItemClick('Home')}
-            style={{ color: getItemColor('Home') }}
-          >
+        <li className="navbar-item" onClick={closeMenu}>
+          <Link to="/portfolio-website" style={{ color: activeItem === 'Home' ? '#E63946' : '#1D3557' }}>
             Home
           </Link>
         </li>
-        <li className="navbar-item">
-          <Link
-            to="/projects"
-            onClick={() => handleItemClick('Projects')}
-            style={{ color: getItemColor('Projects') }}
-          >
+        <li className="navbar-item" onClick={closeMenu}>
+          <Link to="/projects" style={{ color: activeItem === 'Projects' ? '#E63946' : '#1D3557' }}>
             Projects
           </Link>
         </li>
-        <li className="navbar-item">
-          <Link
-            to="/experience"
-            onClick={() => handleItemClick('Experience')}
-            style={{ color: getItemColor('Experience') }}
-          >
+        <li className="navbar-item" onClick={closeMenu}>
+          <Link to="/experience" style={{ color: activeItem === 'Experience' ? '#E63946' : '#1D3557' }}>
             Experience
           </Link>
         </li>
-        <li className="navbar-item">
-          <Link
-            to="/about"
-            onClick={() => handleItemClick('About')}
-            style={{ color: getItemColor('About') }}
-          >
+        <li className="navbar-item" onClick={closeMenu}>
+          <Link to="/about" style={{ color: activeItem === 'About' ? '#E63946' : '#1D3557' }}>
             About
           </Link>
         </li>
